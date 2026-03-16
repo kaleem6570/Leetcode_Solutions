29022e400c82df79006dd4c19e8758e1a5bff309
Syed Kaleemuddin <168353960+kaleem6570@users.noreply.github.com>
2026-03-16 12:04:01 +0530

    [20] [Time Beats: 97.76%] [Memory Beats: 37.91%] - LeetPush

--- Valid-Parentheses.java
+++ Valid-Parentheses.java
@@ -1,31 +1,19 @@
 1class Solution {
 2    public boolean isValid(String p) {
-3       // char a[] = p.toCharArray();
-4        ArrayDeque<Character> s = new ArrayDeque();
-5        for (char c : p.toCharArray()) {
-6            if (c=='{')
-7                s.push('}');
-8           else if (c=='(')
-9                s.push(')');
-10           else if (c=='[')
-11                s.push(']');
-12           else
-13              {
-14                if(s.isEmpty()||s.pop()!=c)
-15                   return false;
-16              }
-17
-18
-19            // else if (s.isEmpty())// if stack is empty 
-20            //     return false;
-21            // else {
-22            //     char s1 = s.peek();// pop the matched element
-23            //     if ((a[i] == ')' && s1 == '(') || (a[i] == '}' && s1 == '{') || (a[i] == ']' && s1 == '['))
-24            //         s.pop();
-25            //     else
-26            //         return false;
-27            // }
-28        }
-29        return s.isEmpty();
-30    }
-31}
+3        ArrayDeque<Character> s = new ArrayDeque();
+4        for (char c : p.toCharArray()) {
+5            if (c=='{')
+6                s.push('}');
+7           else if (c=='(')
+8                s.push(')');
+9           else if (c=='[')
+10                s.push(']');
+11           else
+12              {
+13                if(s.isEmpty()||s.pop()!=c)
+14                   return false;
+15              }
+16        }
+17        return s.isEmpty();
+18    }
+19}
